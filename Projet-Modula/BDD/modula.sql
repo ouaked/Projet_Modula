7a308a409fb0cdd2203f1991a2f4b3da7e96ee48
ouaked <40290234+ouaked@users.noreply.github.com>
2020-03-09 15:40:06 +0100

    Update modula.sql

--- Projet-Modula/BDD/modula.sql
+++ Projet-Modula/BDD/modula.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 06 mars 2020 à 14:04
--- Version du serveur :  5.7.23
--- Version de PHP :  7.2.10
+-- Client :  127.0.0.1
+-- Généré le :  Lun 09 Mars 2020 à 15:31
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `modula`
 --
+CREATE DATABASE IF NOT EXISTS `modula` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `modula`;
 
 -- --------------------------------------------------------
 
@@ -28,16 +28,15 @@ SET time_zone = "+00:00";
 -- Structure de la table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Déchargement des données de la table `admin`
+-- Contenu de la table `admin`
 --
 
 INSERT INTO `admin` (`id`, `login`, `mdp`) VALUES
@@ -49,7 +48,6 @@ INSERT INTO `admin` (`id`, `login`, `mdp`) VALUES
 -- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
@@ -61,10 +59,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `heure` time NOT NULL,
   `ip` varchar(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
 
 --
--- Déchargement des données de la table `user`
+-- Contenu de la table `user`
 --
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `mail`, `message`, `regles`, `date`, `heure`, `ip`) VALUES
@@ -72,7 +70,6 @@ INSERT INTO `user` (`id`, `nom`, `prenom`, `mail`, `message`, `regles`, `date`, 
 (43, 'boussad', 'ouaked', 'boussad@gmail.fr', 'je souhaites intégrer la formation', 'oui', '06-03-2020', '14:01:00', '::1'),
 (44, 'tata', 'tata', 'tata@gmail.com', 'je souhaite intégrer la formation modula', 'oui', '06-03-2020', '14:02:00', '::1'),
 (45, 'jean', 'marc', 'jean@gmail.fr', 'je souhaite intégrer la formation modula', 'oui', '06-03-2020', '14:03:00', '::1');
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
